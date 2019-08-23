f30c4856e8706105881ab724b52114db7cf63b29
Serhii Ostrovetskyi <so@rollncode.com>
2019-08-23 09:54:56 +0300

    delete storyboard

--- TESTGists/TESTGists/AppDelegate.swift
+++ TESTGists/TESTGists/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainController = AuthorizationViewController() as UIViewController
+        let navigationController = UINavigationController(rootViewController: mainController)
+        navigationController.navigationBar.isTranslucent = false
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         
         IQKeyboardManager.shared.enable = true
         return true
